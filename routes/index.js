ca2cb91cc682384d1b0ecf9606b11fcec4da952a
Mike Solomon <msolomon@thrillist.com>
2018-11-03 00:44:32 -0400

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,108 @@
+const credentials = require('../credentials.json')
+const Airtable = require('airtable')
+const base = new Airtable({apiKey: credentials.airtable.apiKey}).base(credentials.airtable.baseId)
+
+
+module.exports = function (app, addon) {
+
+    // Root route. This route will serve the `atlassian-connect.json` unless the
+    // documentation url inside `atlassian-connect.json` is set
+    app.get('/', function (req, res) {
+        res.format({
+            // If the request content-type is text-html, it will decide which to serve up
+            'text/html': function () {
+                res.redirect('/atlassian-connect.json');
+            },
+            // This logic is here to make sure that the `atlassian-connect.json` is always
+            // served up when requested by the host
+            'application/json': function () {
+                res.redirect('/atlassian-connect.json');
+            }
+        });
+    });
+    
+    app.get('/render-airtable-project-status', addon.authenticate(), function (req, res) {
+      if (!req.query.projectId) {
+        res.render('airtable-project-status', {
+          project: {
+            fields: {
+              Name: 'No Project Selected'
+            }
+          }
+        })
+      } else {
+        base('Projects').find(req.query.projectId, function(err, record) {
+          if (err) { console.error(err); return; }
+          console.log(record)
+          res.render('airtable-project-status', {
+            project: record
+          })  
+        })
+      }
+    })
+    
+    app.get('/airtable-project-selector', addon.authenticate(), function (req, res) {
+      res.render('airtable-project-status-selector', {
+        projectId: 'recyREtwXuPw94OEz'
+      })
+    })
+    
+    app.get('/search/project', function (req, res) {
+      res.setHeader('Content-Type', 'application/json');
+      let recordsToReturn = []
+      let projectRetrievalOptions = {
+        view: 'All Projects',
+        sort: [{field: "Name", direction: "desc"}]
+      }
+      if (req.query.q) {
+        projectRetrievalOptions.filterByFormula = `FIND(UPPER("${req.query.q}"), UPPER({Name}), 0)`
+      }
+      base('Projects').select(projectRetrievalOptions).eachPage(function page(records, fetchNextPage) {
+        records.forEach(record => {
+          recordsToReturn.push({
+            label: record.get('Name'),
+            value: record.id
+          })
+        })
+        fetchNextPage()
+      }, function done(err) {
+        if (err) { console.error(err); return (err); }
+        console.log(JSON.stringify(recordsToReturn))
+        res.send(JSON.stringify(recordsToReturn))
+      })
+    })
+
+    // This is an example route that's used by the default "generalPage" module.
+    // Verify that the incoming request is authenticated with Atlassian Connect
+    app.get('/hello-world', addon.authenticate(), function (req, res) {
+            // Rendering a template is easy; the `render()` method takes two params: name of template
+            // and a json object to pass the context in
+            res.render('hello-world', {
+                title: 'Atlassian Connect'
+                //issueId: req.query['issueId']
+            });
+        }
+    );
+
+    // Add any additional route handlers you need for views or REST resources here...
+
+
+    // load any additional files you have in routes and apply those to the app
+    {
+        var fs = require('fs');
+        var path = require('path');
+        var files = fs.readdirSync("routes");
+        for(var index in files) {
+            var file = files[index];
+            if (file === "index.js") continue;
+            // skip non-javascript files
+            if (path.extname(file) != ".js") continue;
+
+            var routes = require("./" + path.basename(file));
+
+            if (typeof routes === "function") {
+                routes(app, addon);
+            }
+        }
+    }
+};
